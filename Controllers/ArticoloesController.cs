e35f0f7c4ff03ed26df96caa05bd6c6e210099fd
zioIgna <ignaziocarbonaro@hotmail.com>
2024-10-11 11:45:39 +0200

    Generato automaticamente controller per classe Articolo

---
+++ Controllers/ArticoloesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gmax.Data;
+using Gmax.Models.Entities;
+
+namespace Gmax.Controllers
+{
+    public class ArticoloesController : Controller
+    {
+        private readonly GmaxDbContext _context;
+
+        public ArticoloesController(GmaxDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Articoloes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Articoli.ToListAsync());
+        }
+
+        // GET: Articoloes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articoli
+                .FirstOrDefaultAsync(m => m.CodiceArticolo == id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articolo);
+        }
+
+        // GET: Articoloes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articoloes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CodiceArticolo,TipoArticolo,Descrizione,UnitaMisura,QtaScortaMin,QtaImpCliente")] Articolo articolo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articolo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articolo);
+        }
+
+        // GET: Articoloes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articoli.FindAsync(id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+            return View(articolo);
+        }
+
+        // POST: Articoloes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CodiceArticolo,TipoArticolo,Descrizione,UnitaMisura,QtaScortaMin,QtaImpCliente")] Articolo articolo)
+        {
+            if (id != articolo.CodiceArticolo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articolo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticoloExists(articolo.CodiceArticolo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articolo);
+        }
+
+        // GET: Articoloes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articolo = await _context.Articoli
+                .FirstOrDefaultAsync(m => m.CodiceArticolo == id);
+            if (articolo == null)
+            {
+                return NotFound();
+            }
+
+            return View(articolo);
+        }
+
+        // POST: Articoloes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var articolo = await _context.Articoli.FindAsync(id);
+            if (articolo != null)
+            {
+                _context.Articoli.Remove(articolo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticoloExists(string id)
+        {
+            return _context.Articoli.Any(e => e.CodiceArticolo == id);
+        }
+    }
+}

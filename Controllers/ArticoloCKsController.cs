eaa367b21c80128d943c58dfe19f357612303e1a
zioIgna <ignaziocarbonaro@hotmail.com>
2024-10-19 18:10:25 +0200

    Creati controller per Entità con chiavi composite

---
+++ Controllers/ArticoloCKsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gmax.Data;
+using Gmax.Models.Entities;
+
+namespace Gmax.Controllers
+{
+    public class ArticoloCKsController : Controller
+    {
+        private readonly GmaxDbContext _context;
+
+        public ArticoloCKsController(GmaxDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ArticoloCKs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.ArticoliCK.ToListAsync());
+        }
+
+        // GET: ArticoloCKs/Details/5
+        public async Task<IActionResult> Details(string tipoArticolo, string codiceArticolo)
+        {
+            if (string.IsNullOrWhiteSpace(tipoArticolo) || string.IsNullOrWhiteSpace(codiceArticolo))
+            {
+                return NotFound();
+            }
+
+            var articoloCK = await _context.ArticoliCK
+                .FirstOrDefaultAsync(m => m.TipoArticolo.Equals(tipoArticolo.Trim()) && m.CodiceArticolo.Equals(codiceArticolo.Trim()));
+            if (articoloCK == null)
+            {
+                return NotFound();
+            }
+
+            return View(articoloCK);
+        }
+
+        // GET: ArticoloCKs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ArticoloCKs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TipoArticolo,CodiceArticolo,Descrizione,UnitaMisuraGestione,QtaScortaMin,CodUbicazione,CodCostruttore,QtaImpCliente,DataInserimento")] ArticoloCK articoloCK)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(articoloCK);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articoloCK);
+        }
+
+        // GET: ArticoloCKs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articoloCK = await _context.ArticoliCK.FindAsync(id);
+            if (articoloCK == null)
+            {
+                return NotFound();
+            }
+            return View(articoloCK);
+        }
+
+        // POST: ArticoloCKs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("TipoArticolo,CodiceArticolo,Descrizione,UnitaMisuraGestione,QtaScortaMin,CodUbicazione,CodCostruttore,QtaImpCliente,DataInserimento")] ArticoloCK articoloCK)
+        {
+            if (id != articoloCK.TipoArticolo)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(articoloCK);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ArticoloCKExists(articoloCK.TipoArticolo))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(articoloCK);
+        }
+
+        // GET: ArticoloCKs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var articoloCK = await _context.ArticoliCK
+                .FirstOrDefaultAsync(m => m.TipoArticolo == id);
+            if (articoloCK == null)
+            {
+                return NotFound();
+            }
+
+            return View(articoloCK);
+        }
+
+        // POST: ArticoloCKs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var articoloCK = await _context.ArticoliCK.FindAsync(id);
+            if (articoloCK != null)
+            {
+                _context.ArticoliCK.Remove(articoloCK);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ArticoloCKExists(string id)
+        {
+            return _context.ArticoliCK.Any(e => e.TipoArticolo == id);
+        }
+    }
+}
---
+++ Controllers/OrdineProduzioneCKsController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gmax.Data;
+using Gmax.Models.Entities;
+
+namespace Gmax.Controllers
+{
+    public class OrdineProduzioneCKsController : Controller
+    {
+        private readonly GmaxDbContext _context;
+
+        public OrdineProduzioneCKsController(GmaxDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrdineProduzioneCKs
+        public async Task<IActionResult> Index()
+        {
+            var gmaxDbContext = _context.OrdiniProduzioneCK
+                .Include(o => o.ArtLancio)
+                .Include(o => o.ArtComponenteList);
+            return View(await gmaxDbContext.ToListAsync());
+        }
+
+        // GET: OrdineProduzioneCKs/Details/5
+        public async Task<IActionResult> Details(int numeroLancio, int numeroSottolancio)
+        {
+            if (numeroLancio == 0 || numeroSottolancio == 0)
+            {
+                return NotFound();
+            }
+
+            var ordineProduzioneCK = await _context.OrdiniProduzioneCK
+                .Include(o => o.ArtLancio)
+                .Include(o => o.ArtComponenteList)
+                .FirstOrDefaultAsync(m => m.NroLancio == numeroLancio && m.NroSottolancio == numeroSottolancio);
+            if (ordineProduzioneCK == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordineProduzioneCK);
+        }
+
+        // GET: OrdineProduzioneCKs/Create
+        public IActionResult Create()
+        {
+            ViewData["TipoArtLancio"] = new SelectList(_context.ArticoliCK, "TipoArticolo", "TipoArticolo");
+            return View();
+        }
+
+        // POST: OrdineProduzioneCKs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("NroLancio,NroSottolancio,Stato,TipoArtLancio,CodArtLancio,DataCreazione,DataPrevCons")] OrdineProduzioneCK ordineProduzioneCK)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ordineProduzioneCK);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TipoArtLancio"] = new SelectList(_context.ArticoliCK, "TipoArticolo", "TipoArticolo", ordineProduzioneCK.TipoArtLancio);
+            return View(ordineProduzioneCK);
+        }
+
+        // GET: OrdineProduzioneCKs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ordineProduzioneCK = await _context.OrdiniProduzioneCK.FindAsync(id);
+            if (ordineProduzioneCK == null)
+            {
+                return NotFound();
+            }
+            ViewData["TipoArtLancio"] = new SelectList(_context.ArticoliCK, "TipoArticolo", "TipoArticolo", ordineProduzioneCK.TipoArtLancio);
+            return View(ordineProduzioneCK);
+        }
+
+        // POST: OrdineProduzioneCKs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("NroLancio,NroSottolancio,Stato,TipoArtLancio,CodArtLancio,DataCreazione,DataPrevCons")] OrdineProduzioneCK ordineProduzioneCK)
+        {
+            if (id != ordineProduzioneCK.NroLancio)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ordineProduzioneCK);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrdineProduzioneCKExists(ordineProduzioneCK.NroLancio))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TipoArtLancio"] = new SelectList(_context.ArticoliCK, "TipoArticolo", "TipoArticolo", ordineProduzioneCK.TipoArtLancio);
+            return View(ordineProduzioneCK);
+        }
+
+        // GET: OrdineProduzioneCKs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ordineProduzioneCK = await _context.OrdiniProduzioneCK
+                .Include(o => o.ArtLancio)
+                .FirstOrDefaultAsync(m => m.NroLancio == id);
+            if (ordineProduzioneCK == null)
+            {
+                return NotFound();
+            }
+
+            return View(ordineProduzioneCK);
+        }
+
+        // POST: OrdineProduzioneCKs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ordineProduzioneCK = await _context.OrdiniProduzioneCK.FindAsync(id);
+            if (ordineProduzioneCK != null)
+            {
+                _context.OrdiniProduzioneCK.Remove(ordineProduzioneCK);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrdineProduzioneCKExists(int id)
+        {
+            return _context.OrdiniProduzioneCK.Any(e => e.NroLancio == id);
+        }
+    }
+}

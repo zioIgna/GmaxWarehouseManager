87e318685a820318513bb0d06250e9d67973169f
zioIgna <ignaziocarbonaro@hotmail.com>
2024-10-26 18:29:36 +0200

    AggiornatoDB

---
+++ Migrations/20241024083517_ShortenedDescrizioneLengthForArticoloCK.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gmax.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShortenedDescrizioneLengthForArticoloCK : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descrizione",
+                table: "ArticoliCK",
+                type: "NVARCHAR2(120)",
+                maxLength: 120,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(130)",
+                oldMaxLength: 130,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Descrizione",
+                table: "ArticoliCK",
+                type: "NVARCHAR2(130)",
+                maxLength: 130,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR2(120)",
+                oldMaxLength: 120,
+                oldNullable: true);
+        }
+    }
+}

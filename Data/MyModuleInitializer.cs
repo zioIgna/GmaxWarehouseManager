4a0a221e7478350f659bb7daf5f485dd55fd6f3d
zioIgna <ignaziocarbonaro@hotmail.com>
2025-04-24 17:40:57 +0200

    Correzione bug relativo a formato data non valido su PostGres #13

---
+++ Data/MyModuleInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Gmax.Data
+{
+    public static class MyModuleInitializer
+    {
+        [ModuleInitializer]
+        public static void Initialize()
+        {
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+    }
+}

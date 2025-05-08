3d5c1fad9f760f4fe534916fa8c25aae5fe7972a
zioIgna <ignaziocarbonaro@hotmail.com>
2025-05-08 12:22:26 +0200

    Migration per relazione Articolo Ordine d'Acquisto #17

--- Migrations/20250508093513_ImpostateRelazioniOrdiniAcq.Designer.cs
+++ Migrations/20250508093513_ImpostateRelazioniOrdiniAcq.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Gmax.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Gmax.Migrations
 {
     [DbContext(typeof(GmaxDbContext))]
-    partial class GmaxDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250508093513_ImpostateRelazioniOrdiniAcq")]
+    partial class ImpostateRelazioniOrdiniAcq
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250508093513_ImpostateRelazioniOrdiniAcq.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Gmax.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImpostateRelazioniOrdiniAcq : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ExpOrdiniAcquisto",
+                table: "ExpOrdiniAcquisto");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ExpOrdiniAcquisto_TipoArticolo_CodiceArticolo_NroOrdineAcqu~",
+                table: "ExpOrdiniAcquisto");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "ExpOrdiniAcquisto");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ExpOrdiniAcquisto",
+                table: "ExpOrdiniAcquisto",
+                columns: new[] { "TipoArticolo", "CodiceArticolo", "NroOrdineAcquisto", "DataConsegnaAcquisto" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExpOrdiniAcquisto_ArticoliCK_TipoArticolo_CodiceArticolo",
+                table: "ExpOrdiniAcquisto",
+                columns: new[] { "TipoArticolo", "CodiceArticolo" },
+                principalTable: "ArticoliCK",
+                principalColumns: new[] { "TipoArticolo", "CodiceArticolo" },
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExpOrdiniAcquisto_ArticoliCK_TipoArticolo_CodiceArticolo",
+                table: "ExpOrdiniAcquisto");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ExpOrdiniAcquisto",
+                table: "ExpOrdiniAcquisto");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "ExpOrdiniAcquisto",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ExpOrdiniAcquisto",
+                table: "ExpOrdiniAcquisto",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpOrdiniAcquisto_TipoArticolo_CodiceArticolo_NroOrdineAcqu~",
+                table: "ExpOrdiniAcquisto",
+                columns: new[] { "TipoArticolo", "CodiceArticolo", "NroOrdineAcquisto", "DataConsegnaAcquisto" },
+                unique: true);
+        }
+    }
+}

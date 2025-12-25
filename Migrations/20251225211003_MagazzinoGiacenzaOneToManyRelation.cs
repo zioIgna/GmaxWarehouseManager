81c47f12359f249bb0403d96aa4f0b43355fa18e
Ignazio Carbonaro <ignaziocarbonaro@hotmail.com>
2025-12-25 22:20:06 +0100

    Migration correzione relazione Magazzino-Giacenza #20

---
+++ Migrations/20251225211003_MagazzinoGiacenzaOneToManyRelation.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gmax.Migrations
+{
+    /// <inheritdoc />
+    public partial class MagazzinoGiacenzaOneToManyRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //migrationBuilder.DropIndex(
+            //    name: "IX_ExpGiacenze_CodMagazzino",
+            //    table: "ExpGiacenze");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpGiacenze_CodMagazzino",
+                table: "ExpGiacenze",
+                column: "CodMagazzino");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ExpGiacenze_CodMagazzino",
+                table: "ExpGiacenze");
+
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_ExpGiacenze_CodMagazzino",
+            //    table: "ExpGiacenze",
+            //    column: "CodMagazzino",
+            //    unique: true);
+        }
+    }
+}
